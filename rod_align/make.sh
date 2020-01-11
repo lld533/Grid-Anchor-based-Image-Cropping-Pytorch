9d342ee3dee9dfdf62099a89bfaa5678ea0595ed
tomotana14 <tomotana14@gmail.com>
2020-01-11 16:20:54 +0900

    fix

--- rod_align/make.sh
+++ rod_align/make.sh
@@ -6,7 +6,7 @@ echo "Compiling rod_align kernels by nvcc..."
 # -arch=sm_75 is compatible with the following NV GPU cards,
 # GeForce RTX 2080 Ti, RTX 2080, RTX 2070    Quadro RTX 8000, Quadro RTX 6000, Quadro RTX 5000 Tesla T4
 # See more at https://raw.githubusercontent.com/stereolabs/zed-yolo/master/libdarknet/Makefile
-nvcc -c -o rod_align_kernel.cu.o roi_align_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_75
+nvcc -c -o rod_align_kernel.cu.o rod_align_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_75
 
 cd ../
 # Export CUDA_HOME. And build and install the library.
